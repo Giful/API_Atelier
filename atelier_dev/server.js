5780ea5c8a26876eb00cc234fd4a4b354f2afe9d
Antoine <antoine.gachenot1@etu.univ-lorraine.fr>
2020-03-16 12:45:31 +0100

    création du docker et de la bdd

---
+++ atelier_dev/server.js
@@ -0,0 +1,70 @@
+"use strict";
+
+const bodyparser = require('body-parser');
+const express = require("express");
+const mysql = require("mysql");
+const uuid = require("uuid/v1");
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const validator = require('validator');
+const cors = require('cors');
+
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+const app = express();
+
+app.use(bodyparser.json());
+app.use(
+    bodyparser.urlencoded({
+        extended: false
+    })
+);
+
+var corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            console.warn("Warning : Missing 'Origin' header")
+        }
+        callback(null, true)
+    }
+}
+app.use(cors(corsOptions))
+
+// GET
+
+app.get("/", (req, res) => {
+    res.send("Atelier API\n");
+});
+
+
+// Les autres méthodes ne sont pas allowed
+
+app.all("/*", (req, res) => {
+    let erreur = {
+        "type": "error",
+        "error": 400,
+        "message": "BAD REQUEST"
+    }
+    JSON.stringify(erreur)
+    res.send(erreur)
+});
+
+app.listen(PORT, HOST);
+console.log(`Atelier API Running on http://${HOST}:${PORT}`);
+
+const db = mysql.createConnection({
+    host: "mysql.atelier",
+    user: "atelier_bdd",
+    password: "atelier_bdd",
+    database: "atelier_bdd"
+});
+
+db.connect(err => {
+    if (err) {
+        console.error(err);
+    }
+    console.log("Connected to database");
+});
+
+// Pour y accéder, port 19080
