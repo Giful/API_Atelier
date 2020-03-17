e888b911905e232cc7eef4abe6c3e79cd2c0a1f3
Antoine <antoine.gachenot1@etu.univ-lorraine.fr>
2020-03-17 16:50:46 +0100

    ajout post route joueur

--- atelier_dev/server.js
+++ atelier_dev/server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const mysql = require("mysql");
 const uuid = require("uuid/v1");
 const axios = require('axios');
+const passwordHash = require('password-hash');
 const jwt = require('jsonwebtoken');
 const validator = require('validator');
 const cors = require('cors');
@@ -339,138 +340,6 @@ app.get('/photos/:id', function (req, res) {
     });
 });
 
-app.get("/parties", function (req, res) {
-    let page = req.param('page');
-    if (typeof page === 'undefined' || page <= 0) page = 1;
-    let debutLimit = (page - 1) * 10;
-    let queryParties = `SELECT * FROM partie limit ${debutLimit}, 10`;
-
-    let count = 0;
-    db.query(queryParties, (errParties, resultParties) => {
-        if (errParties) console.log(errParties);
-        else {
-            count = resultParties.length;
-
-            let pageMax = Math.ceil(count / 10);
-            if (page > pageMax) page = pageMax;
-
-            let next = parseInt(page) + 1;
-            if (next > pageMax) next = pageMax;
-
-            let prev = page - 1;
-            if (prev < 1) prev = 1;
-
-            resultParties.forEach(function (p, index) {
-                resultParties[index] = JSON.parse(JSON.stringify({
-                    parties: p,
-                    links: { self: { href: "/parties/" + p.idPartie } }
-                }));
-            });
-            res.json({
-                "type": "collection",
-                "count": count,
-                "size": 10,
-                "links": {
-                    "next": {
-                        "href": "/parties/?page=" + next
-                    },
-                    "prev": {
-                        "href": "/parties/?page=" + prev
-                    },
-                    "last": {
-                        "href": "/parties/?page=" + pageMax
-                    },
-                    "first": {
-                        "href": "/parties/?page=1"
-                    },
-                },
-                "parties": resultParties
-            });
-        }
-    });
-});
-
-app.get('/parties/:id', function (req, res) {
-
-    let queryPartiesId = `SELECT * FROM partie WHERE idPartie = ${req.params.id}`;
-
-    db.query(queryPartiesId, (errPartieId, resultPartieId) => {
-        if (errPartieId) {
-            let erreur = {
-                "type": "error",
-                "error": 500,
-                "message": err
-            };
-        } else if (resultPartieId == "") {
-            let erreur = {
-                "type": "error",
-                "error": 404,
-                "message": req.params.id + " n'est pas valide"
-            };
-            JSON.stringify(erreur);
-            res.send(erreur);
-        } else {
-            let querySerieId = `SELECT * FROM serie WHERE idSerie = ${resultPartieId[0].refSerie}`;
-            db.query(querySerieId, (errSerieId, resultSerieId) => {
-                if (errSerieId) {
-                    let erreur = {
-                        "type": "error",
-                        "error": 500,
-                        "message": err2
-                    };
-                    JSON.stringify(erreur);
-                    res.send(erreur);
-                } else if (resultSerieId === "") {
-                    let erreur = {
-                        "type": "error",
-                        "error": 404,
-                        "message": req.params.id + " isn't a valid id"
-                    };
-                    JSON.stringify(erreur);
-                    res.send(erreur);
-                } else {
-                    let queryJoueurId = `SELECT * FROM joueur WHERE idJoueur = ${resultPartieId[0].refJoueur}`;
-                    db.query(queryJoueurId, (errJoueurId, resultJoueurId) => {
-                        if (errJoueurId) {
-                            let erreur = {
-                                "type": "error",
-                                "error": 500,
-                                "message": err2
-                            };
-                            JSON.stringify(erreur);
-                            res.send(erreur);
-                        } else if (resultJoueurId === "") {
-                            let erreur = {
-                                "type": "error",
-                                "error": 404,
-                                "message": req.params.id + " isn't a valid id"
-                            };
-                            JSON.stringify(erreur);
-                            res.send(erreur);
-                        } else {
-                            res.json({
-                                "type": "ressource",
-                                "links": {
-                                    "self": "/parties/" + req.params.id,
-                                    "joueur" : "/joueurs/" + resultPartieId[0].refJoueur,
-                                    "serie" : "/series/" + resultPartieId[0].refSerie
-                                },
-                                "partie": {
-                                    "id": resultPartieId[0].idPartie,
-                                    "created_at": resultPartieId[0].created_at,
-                                    "statut": resultPartieId[0].statut,
-                                    "score": resultPartieId[0].score,
-                                    "serie": resultSerieId,
-                                    "joueur": resultJoueurId,
-                                }
-                            });
-                        }
-                    });
-                }
-            });
-        };
-    });
-});
 
 
 
@@ -499,6 +368,19 @@ app.post("/photo", (req, res) => {
         }
     })
 })
+
+app.post("/joueur", (req,res) => {
+   let pwd = passwordHash.generate(req.body.mdp)
+   let dateAct = new Date().toJSON().slice(0, 19).replace('T', ' ')
+   db.query(`INSERT INTO joueur (mail,pseudo,password,created_at,updated_at) VALUES ("${req.body.mail}","${req.body.pseudo}","${pwd}","${dateAct}","${dateAct}")`, (err,result) => {
+       if(err) {
+           console.error(err);
+           res.status(500).send(JSON.stringify(err));
+       }else{
+           res.status(201).json(req.body)
+       }
+   })
+});
 
         // Les autres méthodes ne sont pas allowed
 
