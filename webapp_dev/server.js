a22327c410f25eefed358cfac0cf6c24537f89e5
Axel Gand <axel.gand5@etu.univ-lorraine.fr>
2020-03-20 18:54:57 +0100

    ajout api webapp_dev

---
+++ webapp_dev/server.js
@@ -0,0 +1,143 @@
+"use strict";
+
+const bodyparser = require('body-parser');
+const express = require("express");
+const mysql = require("mysql");
+const axios = require('axios');
+const passwordHash = require('password-hash');
+const jwt = require('jsonwebtoken');
+const validator = require('validator');
+const cors = require('cors');
+
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+const app = express();
+
+app.use(bodyparser.json());
+app.use(
+    bodyparser.urlencoded({
+        extended: false
+    })
+);
+
+var corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            console.warn("Warning : Missing 'Origin' header")
+        }
+        callback(null, true)
+    }
+}
+app.use(cors(corsOptions))
+
+// GET
+
+app.get("/", (req, res) => {
+    res.send("WebApp API\n");
+});
+
+app.get('/series', function (req, res) {
+    let token = null;
+
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] == "Bearer") token = req.headers.authorization.split(' ')[1];
+
+    if(token != null) {
+        let querySeries = `SELECT * FROM serie order by ville ASC`;
+
+        db.query(querySeries, (errSeries, resultSeries) => {
+            if (errSeries) {
+                let erreur = {
+                    "type": "error",
+                    "error": 500,
+                    "message": errSeries
+                };
+                JSON.stringify(erreur);
+                res.send(erreur);
+            }
+            else {
+                resultSeries.forEach(function (s, index) {
+                    resultSeries[index] = JSON.parse(JSON.stringify({
+                        serie: s
+                    }));
+                });
+
+                res.json({
+                    "type": "collection",
+                    "series": resultSeries
+                });
+            }
+        });
+    } else res.status(400).json({"type": "error","error": 400,"message": "Aucun Bearer Token en paramètre Header"});
+});
+
+// POST
+
+app.post("/joueurs/auth", (req, res) => {
+    let mail, password;
+
+    if(req.headers.authorization) {
+        const base64Credentials = req.headers.authorization.split(' ')[1]
+        const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii')
+        mail = credentials.split(':')[0]
+        password = credentials.split(':')[1]
+
+        db.query(`select mail, password from joueur where mail = "${mail}"`, (err, result) => {
+            if (err) {
+                let erreur = {
+                    "type": "error",
+                    "error": 500,
+                    "message": err
+                };
+                JSON.stringify(erreur);
+                res.send(erreur);
+            } else if (result.length == 0) {
+                let erreur = {
+                    "type": "error",
+                    "error": 404,
+                    "message": mail + " n'existe pas"
+                };
+                JSON.stringify(erreur);
+                res.send(erreur);
+            } else {
+                if(mail == result[0].mail && passwordHash.verify(password, result[0].password)) {
+                    let token = jwt.sign({}, 'privateKeyApi', {algorithm: 'HS256'})
+                    res.json({token: token})
+                } else res.status(401).json({"type": "error","error": 401,"message": "Mauvaise adresse mail ou mot de passe"})
+            }
+        })
+    } else res.status(401).json({"type": "error","error": 401,"message": "Aucune Authorization Basic Auth"})
+})
+
+// Les autres méthodes ne sont pas allowed
+
+app.all("/*", (req, res) => {
+    let erreur = {
+        "type": "error",
+        "error": 400,
+        "message": "BAD REQUEST"
+    }
+    JSON.stringify(erreur)
+    res.send(erreur)
+});
+
+app.listen(PORT, HOST);
+console.log(`Mobile API Running on http://${HOST}:${PORT}`);
+
+const db = mysql.createConnection({
+    host: "mysql.atelier",
+    user: "atelier_bdd",
+    password: "atelier_bdd",
+    database: "atelier_bdd"
+});
+
+db.connect(err => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("Connected to database");
+    }
+
+});
+
+// Pour y accéder, port 19280
